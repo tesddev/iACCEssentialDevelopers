6164e63fd3bd527ae1369023171d6370efa3c6bd
tesddev <tesleem.amuda@gmail.com>
2023-01-25 13:56:20 +0100

    extract reusable logics to extension of uiview controller

--- iACC/ListViewController.swift
+++ iACC/ListViewController.swift
@@ -170,50 +170,6 @@ class ListViewController: UITableViewController {
             fatalError("unknown item: \(item)")
         }
     }
-    
-    func select(friend: Friend) {
-        let vc = FriendDetailsViewController()
-        vc.friend = friend
-        show(vc, sender: self)
-    }
-    
-    func select(card: Card) {
-        let vc = CardDetailsViewController()
-        vc.card = card
-        show(vc, sender: self)
-    }
-    
-    func select(transfer: Transfer) {
-        let vc = TransferDetailsViewController()
-        vc.transfer = transfer
-        show(vc, sender: self)
-    }
-    
-    @objc func addCard() {
-        let vc = AddCardViewController()
-        show(vc, sender: self)
-    }
-    
-    @objc func addFriend() {
-        let vc = AddFriendViewController()
-        show(vc, sender: self)
-    }
-    
-    @objc func sendMoney() {
-        let vc = SendMoneyViewController()
-        show(vc, sender: self)
-    }
-    
-    @objc func requestMoney() {
-        let vc = RequestMoneyViewController()
-        show(vc, sender: self)
-    }
-    
-    func show(error: Error) {
-        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .default))
-        self.showDetailViewController(alert, sender: self)
-    }
 }
 
 extension UITableViewCell {
@@ -268,5 +224,51 @@ struct ItemViewModel {
     init(card: Card){
         title = card.number
         subtitle = card.holder
+    }
+}
+
+extension UIViewController {
+    func select(friend: Friend) {
+        let vc = FriendDetailsViewController()
+        vc.friend = friend
+        show(vc, sender: self)
+    }
+    
+    func select(card: Card) {
+        let vc = CardDetailsViewController()
+        vc.card = card
+        show(vc, sender: self)
+    }
+    
+    func select(transfer: Transfer) {
+        let vc = TransferDetailsViewController()
+        vc.transfer = transfer
+        show(vc, sender: self)
+    }
+    
+    @objc func addCard() {
+        let vc = AddCardViewController()
+        show(vc, sender: self)
+    }
+    
+    @objc func addFriend() {
+        let vc = AddFriendViewController()
+        show(vc, sender: self)
+    }
+    
+    @objc func sendMoney() {
+        let vc = SendMoneyViewController()
+        show(vc, sender: self)
+    }
+    
+    @objc func requestMoney() {
+        let vc = RequestMoneyViewController()
+        show(vc, sender: self)
+    }
+    
+    func show(error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        self.showDetailViewController(alert, sender: self)
     }
 }
