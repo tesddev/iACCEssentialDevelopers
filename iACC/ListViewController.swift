a06528cf49ca9587cefd9028925a79f0ed5a56b1
tesddev <tesleem.amuda@gmail.com>
2023-01-25 13:03:10 +0100

    copy configure method in cell into model and make it an initialiser

--- iACC/ListViewController.swift
+++ iACC/ListViewController.swift
@@ -233,4 +233,34 @@ extension UITableViewCell {
 struct ItemViewModel {
     let title: String
     let subtitle: String
+    
+    init(_ item: Any, longDateStyle: Bool) {
+        if let friend = item as? Friend {
+            title = friend.name
+            subtitle = friend.phone
+        } else if let card = item as? Card {
+            title = card.number
+            subtitle = card.holder
+        } else if let transfer = item as? Transfer {
+            let numberFormatter = Formatters.number
+            numberFormatter.numberStyle = .currency
+            numberFormatter.currencyCode = transfer.currencyCode
+            
+            let amount = numberFormatter.string(from: transfer.amount as NSNumber)!
+            title = "\(amount) • \(transfer.description)"
+            
+            let dateFormatter = Formatters.date
+            if longDateStyle {
+                dateFormatter.dateStyle = .long
+                dateFormatter.timeStyle = .short
+                subtitle = "Sent to: \(transfer.recipient) on \(dateFormatter.string(from: transfer.date))"
+            } else {
+                dateFormatter.dateStyle = .short
+                dateFormatter.timeStyle = .short
+                subtitle = "Received from: \(transfer.sender) on \(dateFormatter.string(from: transfer.date))"
+            }
+        } else {
+            fatalError("unknown item: \(item)")
+        }
+    }
 }
